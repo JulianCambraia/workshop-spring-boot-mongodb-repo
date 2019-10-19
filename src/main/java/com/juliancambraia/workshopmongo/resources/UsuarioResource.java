4fc47572bf9efd2a3f6d2ae740d42bb7721c50b7
JulianFernando <juliancambraia@gmail.com>
2019-10-19 12:03:57 -0300

    Obtendo um Usuário por ID com tratamento de Exceção personalizada

--- src/main/java/com/juliancambraia/workshopmongo/resources/UsuarioResource.java
+++ src/main/java/com/juliancambraia/workshopmongo/resources/UsuarioResource.java
@@ -1,11 +1,11 @@
 package com.juliancambraia.workshopmongo.resources;
 
-import com.juliancambraia.workshopmongo.domain.Usuario;
 import com.juliancambraia.workshopmongo.dto.UsuarioDTO;
 import com.juliancambraia.workshopmongo.services.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,9 +17,16 @@ public class UsuarioResource {
 
     @Autowired
     private UsuarioService usuarioService;
+
     @GetMapping
     public ResponseEntity<List<UsuarioDTO>> findAll() {
         List<UsuarioDTO> list = usuarioService.findAll();
         return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<UsuarioDTO> findById(@PathVariable String id) {
+        UsuarioDTO usuario = usuarioService.findById(id);
+        return ResponseEntity.ok().body(usuario);
     }
 }
--- src/main/java/com/juliancambraia/workshopmongo/services/UsuarioService.java
+++ src/main/java/com/juliancambraia/workshopmongo/services/UsuarioService.java
@@ -4,10 +4,12 @@ import com.juliancambraia.workshopmongo.domain.Usuario;
 import com.juliancambraia.workshopmongo.dto.UsuarioDTO;
 import com.juliancambraia.workshopmongo.mappers.UsuarioMapper;
 import com.juliancambraia.workshopmongo.repository.UsuarioRepository;
+import com.juliancambraia.workshopmongo.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UsuarioService {
@@ -21,5 +23,10 @@ public class UsuarioService {
     public List<UsuarioDTO> findAll() {
         List<Usuario> usuarios = usuarioRepository.findAll();
         return usuarioMapper.toDtos(usuarios);
+    }
+
+    public UsuarioDTO findById(String id) {
+        Optional<Usuario> usuario = Optional.ofNullable(usuarioRepository.findById(id).orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado!")));
+        return usuarioMapper.toDto(usuario.get());
     }
 }
